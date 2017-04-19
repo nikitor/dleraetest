680cf2c24636d6e613a5edc381f8fc8883a942c7
nikitor <tamote@yandex.com>
2017-04-19 23:35:53 +0200

    añadi la funcion 'géneros' que construye la combinacion de palabras, para cada género a partir de las terminaciones especificadas:

--- scratch.py
+++ scratch.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import re
 import requests
 import json
@@ -8,13 +9,13 @@ import json
 headrs = {'Authorization': 'Basic cDY4MkpnaFMzOmFHZlVkQ2lFNDM0', 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'}
 # palabras=["calibre","rinoceronte","elefante","caca","kaka","vacuno"]
 # palabras=["vacuno","elefante"]
-palabras=["doctora"]
+palabras=['elegida']
 for palabra in palabras :
     r = requests.get('https://dle.rae.es/data/search?w=' + palabra, headers=headrs)
     print(r.status_code)
 
     # save response json for word to its own file
-    with open(palabra + '.json', 'w') as outfile:
+    with open("./json/"+palabra + '.json', 'w') as outfile:
         json.dump(r.json(), outfile)
 
     res = r.json()["res"]
@@ -26,3 +27,15 @@ for palabra in palabras :
     # print(palabra + ":" + str(any(i["header"] == palabra + "." for i in r.json()["res"])))
     # print(palabra + ":" + str(any(i["header"] == palabra + "." for i in r.json()["res"])))
     print(palabra + ":" + str(any(rxp.search(i["header"]) for i in res)))
+
+# función que devuelve el set de palabras generadas,
+# tomando en cuenta las posibles terminaciones de género
+#
+# Ejemplo:
+#
+# >>> generos('elegido, da.')
+# ['elegido', 'elegida']
+
+def generos(s) :
+    t = s.rpartition(', ')
+    return [t[0], t[0][0:t[0].rfind(t[2][0])] + t[2].rstrip('.')]
